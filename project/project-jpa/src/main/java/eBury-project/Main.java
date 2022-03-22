92b7cf5330124469a2f392327ea82a23aa3f3403
psychys <swaglord@uma.es>
2022-03-22 09:09:55 +0100

    Arquitectura creada correctamente

---
+++ project/project-jpa/src/main/java/eBury-project/Main.java
@@ -0,0 +1,18 @@
+package eBury-project;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Main {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("project");
+		EntityManager em = emf.createEntityManager();
+		
+		em.close();
+		emf.close();
+
+	}
+
+}
