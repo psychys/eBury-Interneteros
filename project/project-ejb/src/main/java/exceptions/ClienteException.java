20f3a60d0f44c2234109c94f100be333ad425d60
psychys <swaglord@uma.es>
2022-04-22 17:52:35 +0200

    Añadido paquete exceptions

---
+++ project/project-ejb/src/main/java/exceptions/ClienteException.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+public class ClienteException extends Exception{
+
+    public ClienteException(){
+
+    }
+
+    public ClienteException(String msg){
+        super(msg);
+    }
+
+}
