c5efb77bcfc2890fc8c686d16792bd6a4ba409a5
psychys <swaglord@uma.es>
2022-04-22 18:08:03 +0200

    GestionCliente con metodos añadidos

--- project/project-ejb/src/main/java/GestionCliente.java
+++ project/project-ejb/src/main/java/GestionCliente.java
@@ -1,9 +1,19 @@
+import eBury_project.Cliente;
+
+public interface GestionCliente {
+
+    //Requisito 2
+    public void CrearCliente(Cliente c);
+
+    //Requisito 3
+    public void ActualizarCliente(Cliente c);
+
+    public void LeerCliente(Cliente c);
+
+    //Requisito 4
+    public void MarcarCliente(Cliente c);
 
 
-
-
-
-public class GestionCliente {
 
 
 }
