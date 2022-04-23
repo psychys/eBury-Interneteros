3ec047912c99fb9e3499ed177c288c0c95831297
carlosqc117 <quesadacarlos117@gmail.com>
2022-04-23 17:13:02 +0200

    CuentaException

---
+++ project/project-ejb/src/main/java/exceptions/CuentaException.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+public class CuentaException extends Exception{
+
+    public CuentaException(){
+
+    }
+    public CuentaException(String msg){
+        super(msg);
+    }
+}
