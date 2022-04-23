937ba7a4043c2b31e69a9e000ab033fcd24c2e99
xVenom <62355097+iliasm000@users.noreply.github.com>
2022-04-23 12:04:44 +0200

    Creada excepcion de divisa

---
+++ project/project-ejb/src/main/java/exceptions/DivisaException.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+public class DivisaException extends Exception{
+
+    public DivisaException(){
+
+    }
+
+    public DivisaException(String msg){
+        super(msg);
+    }
+
+}
